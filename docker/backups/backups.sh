d3884850b7e692b1ea07881586b55177c1275388
Djax Bank <djax@djax.dev>
2026-08-05 14:59:20 +0200

    basic working backup script (backs nothing up tho)

--- docker/backups/backups.sh
+++ docker/backups/backups.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-
+set -e 
 while true ; do 
     echo "backup in progress"
-    folder=$(date "+Backup-%D-%H:%M")
+    folder=$(date "+Backup-%Y-%m-%d-%H-%M")
     archive=${folder}.tar.gz
 
-    mkdir "$(folder)" tmp
-    tar -czvf "$(archive)" "$(folder)"
-    rm -rf "$(folder)"
-    mv "$(archive)" backups
-    
-    sleep 600
+    mkdir -p "$folder" tmp
+    tar -czvf "$archive" "$folder"
+    rm -rf "$folder" tmp
+    mv "$archive" ./backups
+
+
+    cd backups 
+    ls | sort | head -n -2 | xargs -r rm 
+    cd ../ 
+
+    sleep 60
 done
