fdd770b7c273d3a1a810a243c21cc925a167c1d0
Djax Bank <djax@djax.dev>
2026-08-06 13:38:56 +0200

    fix backup.sh

--- docker/backups/backups.sh
+++ docker/backups/backups.sh
@@ -3,18 +3,18 @@ set -e
 while true ; do 
     echo "backup in progress"
     archive=$(date "+Backup-%Y-%m-%d-%H-%M").tar.gz
-
     mkdir -p tmp
-    cd tmp
 
+
+    cd tmp
     pg_dump -h postgres -U "$POSTGRES_USER" -d "$POSTGRES_DB" > postgres.sql
 
+    
+    cd ../
+    tar -czvf "backups/$archive" tmp/*
+    rm -rf tmp
 
-    tar -czvf "$archive" tmp
-    rm -rf "$folder" tmp
-    mv "$archive" ./backups
-
-    cd ../backups 
+    cd backups 
     ls | sort | head -n -10 | xargs -r rm 
     cd ../ 
 
