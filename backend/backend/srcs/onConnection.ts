a3a8cb5e2c35acfa77164f1204cebc0e4582cdd9
VivienSoulas <vivien.soulas@gmail.com>
2026-08-06 12:19:36 +0200

    game, longin, registration, email validation, cookie session working

--- backend/backend/srcs/onConnection.ts
+++ backend/backend/srcs/onConnection.ts
@@ -1,9 +1,6 @@
-
 import { createRoom } from "./rooms/gameRoom.js"
-import { rooms, players, mockDB, type Room , type Player } from "./state/gameState.js"
+import { rooms, players } from "./state/gameState.js"
 import { PLAYER_DEFAULT_HP, PLAYER_DEFAULT_X, PLAYER_DEFAULT_Y, ROOM_MAX_SIZE } from "./constants.js"
-import type { Spawn, Vec2 } from "./types.js"
-import type { Socket } from "socket.io"
 
 const findAvailableRoom = () => {
   return Object.entries(rooms).find(
@@ -11,35 +8,20 @@ const findAvailableRoom = () => {
   ) || null
 }
 
-const getPlayerFromDB = (dataId: number) => {
-	//TODO: change it to fetch API alkuijte (auth)
-	return mockDB.players[dataId] || null
-}
+const createPlayer = (socket, user, slot, spawn) => {
 
-// Turn : any into more exact data types
-const createPlayer = (socket: Socket, data: any, slot: any, spawn: Spawn ) => {
-	
-	const hasPlayer = getPlayerFromDB(data.id)
-
-	if (hasPlayer)
-		return {...hasPlayer, socketID: socket.id}
-
-	console.log('Player not found, creating new') //debbug
-
-	const player: Player = {
-		userId: data.id,
-		socketId: socket.id,
-		username: data.username,
+	return {
+		userID: user.id,
+		socketID: socket.id,
+		username: user.username,
 		slot,
 		hp: PLAYER_DEFAULT_HP,
-		x: spawn.pos.x,
-		y: spawn.pos.y
-	}
+		x: spawn.x,
+		y: spawn.y
+	};
+};
 
-	return player;
-}
-
-const findAvailableSlot = (room: Room, maxSize: number) => {
+const findAvailableSlot = (room, maxSize) => {
 	const usedSlots = new Set(room.players.map(p => p.slot))
 
 	for (let slot = 1; slot <= maxSize; slot++) {
@@ -49,11 +31,11 @@ const findAvailableSlot = (room: Room, maxSize: number) => {
 	return null
 }
 
-const onJoin = (socket: Socket, data: any) => {
+const onJoin = (socket, data) => {
 
 	let available = findAvailableRoom()
-	let room: Room
-	let roomId: string
+	let room
+	let roomId
 
 	if (!available) {
 		const [newRoom, newRoomId] = createRoom()
@@ -71,36 +53,9 @@ const onJoin = (socket: Socket, data: any) => {
 		return
 	}
 
-	// inform DB about the new player on that room
-	// Flima please double check this code, I am not sure if it has the intended bevaviour
-	if (!mockDB.rooms[roomId]) {
-		mockDB.rooms[roomId] = {
-			roomId: roomId,
-			playerCount: room.playerCount,
-			players: room.players,
-			map: room.map,
-		}
-	}
-	mockDB.rooms[roomId] = {
-		roomId: roomId,
-		playerCount: room.playerCount + 1,
-		players: room.players,
-		map: room.map,
-	}
+	const spawn = room.map.spawnPoints.find(sp => sp.playerSlot === slot)
 
-	// Rework this bit to be easier to read and ideally avoid the tspawn var
-	const tspawn = room.map.spawnPoints.find(sp => sp.playerSlot === slot)
-	if (tspawn == undefined) {
-		console.error(`Unable to find a spawn location`)
-		socket.emit('join_error', { message: 'Unable to find a spawn location' })
-		return
-	}
-	const spawn = {
-		playerId: tspawn.playerSlot,
-		pos: { x: tspawn.x, y: tspawn.y },
-	};
-
-	const player = createPlayer(socket, data, slot, spawn)
+	const player = createPlayer(socket, socket.user, slot, spawn)
 	players[socket.id] = player
 	room.players.push(player)
 
@@ -115,40 +70,49 @@ const onJoin = (socket: Socket, data: any) => {
 	return roomId
 }
 
-const onDisconnection = (socket: Socket, roomId: string) => {
+const onDisconnection = (socket, roomId) => {
 	const player = players[socket.id]
 	if (!player) return
 
 	delete players[socket.id]
 
-	const room = rooms[roomId]
-	if (!room) return
+	if (!rooms[roomId]) return
 
-	room.players = room.players.filter(p => p.socketId !== socket.id)
+	// remove da sala
+	rooms[roomId].players = rooms[roomId].players.filter(
+		p => p.socketID !== socket.id
+	)
 
-	if (room.players.length === 0) {
-	  delete rooms[roomId]
-	  delete mockDB.rooms[roomId]
-	  console.log("Room deleted:", roomId)
+	if (rooms[roomId].players.length === 0) {
+		// sala vazia — deleta
+		delete rooms[roomId]
+		console.log('Room deleted:', roomId)
 	} else {
-	  const dbRoom = mockDB.rooms[roomId]
-	  if (!dbRoom) return
-
-	  dbRoom.playerCount = (dbRoom.playerCount ?? 0) - 1;
-	  socket.to(roomId).emit('player_left', player.username);
+		socket.to(roomId).emit('player_left', player.username)
 	}
 }
 
-const onConnection = async (socket: Socket) => {
-	console.log('Player logged in: ', socket.id)
-	let currentroomId: string;
+const onConnection = async (socket) => {
 
-	socket.on('join', (data) => {
-		currentroomId = onJoin(socket, data)! // Potentially causes bugs if Id is null
-	})
+	console.log(
+		"Player connected:",
+		socket.user.username
+	);
+	let currentroomId = null;
+
+	socket.on('join', () => {
+		currentroomId = onJoin(socket);
+	});
+
 	socket.on('disconnect', () => {
-		console.log('Player logged out:', socket.id)
-		onDisconnection(socket, currentroomId)
-	})
-}
-export default onConnection
+
+		console.log(
+			"Player disconnected:",
+			socket.user.username
+		);
+
+		onDisconnection(socket, currentroomId);
+	});
+};
+
+export default onConnection;
--- backend/backend/srcs/rooms/gameRoom.ts
+++ backend/backend/srcs/rooms/gameRoom.ts
@@ -1,34 +1,27 @@
-import { randomUUID } from 'node:crypto'
-import {  rooms, mockDB } from "../state/gameState.js"
-import type { Room } from "../state/gameState.js"
-import { generateMap } from '../map/mapGenerator.js'
-import { ROOM_MAX_SIZE } from '../constants.js'
+import { randomUUID } from "node:crypto";
+import { rooms } from "../state/gameState.js";
+import { generateMap } from "../map/mapGenerator.js";
+import { ROOM_MAX_SIZE } from "../constants.js";
 
-const roomId: string  = randomUUID();
-
-rooms[roomId] = {
-  roomId: roomId,
-  playerCount: 0,
-  players: [],
-  map: generateMap(ROOM_MAX_SIZE),
-};
 
 const createRoom = () => {
-	const roomId: string = randomUUID();
-	const room: Room = {
-		roomId: roomId,
-		playerCount: 0,
-  		players: [],
-  		map: generateMap(ROOM_MAX_SIZE),
+
+	const roomId = randomUUID();
+
+	rooms[roomId] = {
+		players: [],
+		map: generateMap(ROOM_MAX_SIZE)
 	};
-	rooms[roomId] = room;
-
-	//TODO: change it to fetch API alkuijte  - POST newroom
-	// mockDB.rooms[roomId] = { roomId, playerCount: 0 } This line does not make any sense to me
-	console.log('Room created:', roomId) //debbug
 
 
-	return [room, roomId] as const;
-}
+	console.log("Room created:", roomId);
 
-export { createRoom }
+
+	return [
+		rooms[roomId],
+		roomId
+	];
+};
+
+
+export { createRoom };
