02de797d67e28b0db26fbd6abded2e6398f240f3
VivienSoulas <vivien.soulas@gmail.com>
2026-08-06 17:08:18 +0200

    auto log in after email verification

--- docker/postgres/init/01_schema.sql
+++ docker/postgres/init/01_schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE IF NOT EXISTS users (
 	verification_token TEXT,
 	verification_expires_at TIMESTAMPTZ,
 	password_hash TEXT NOT NULL,
+	enabled_2FA BOOLEAN NOT NULL DEFAULT FALSE,
+	TOTP_secret TEXT,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
