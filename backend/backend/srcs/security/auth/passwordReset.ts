0c52b0cbd3d3bfd4d46475accd05719abddd0f27
VivienSoulas <vivien.soulas@gmail.com>
2026-08-14 16:06:39 +0200

    creation of reset password page and backend

--- backend/backend/srcs/security/auth/passwordReset.ts
+++ backend/backend/srcs/security/auth/passwordReset.ts
@@ -1,55 +1,69 @@
-//import bcrypt from "bcrypt";
-//import { insertUser } from "../repository/userRepository.js";
-//import nodemailer from "nodemailer";
+import { insertUserPasswordVerification } from "../repository/userRepository.js";
+import { randomBytes, createHash } from "node:crypto";
+import nodemailer from "nodemailer";
 
-//export const emailTransporter = nodemailer.createTransport({
-//	service: "gmail",
-//	auth: {
-//		user: process.env.EMAIL_USER,
-//		pass: process.env.EMAIL_PASSWORD,
-//	},
-//});
+export const emailTransporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASSWORD,
+	},
+});
 
-//type passwordResetPayload = {
-//	username: string;
-//	email: string;
-//}
-//export async function passwordReset(payload: passwordResetPayload) {
-//	const { username, email } = payload;
-//	if (!username || !email) {
-//		return {
-//			ok:false,
-//			statusCode:400,
-//			message:"Missing fields",
-//		};
-//	}
-//	const baseUrl = process.env.HOSTNAME;
-//	const verificationUrl =
-//	`https://${baseUrl}:8443/verify-email?token=${verification_token}`;
+export async function passwordResetRequest(email: string) {
+	if (!email) {
+		return {
+			ok:false,
+			statusCode:400,
+			message:"Missing fields",
+		};
+	}
+	const password_verification_token = randomBytes(32).toString("hex");
+	const password_verification_token_hash = createHash("sha256")
+		.update(password_verification_token)
+		.digest("hex");
 
-//	try {
-//		await emailTransporter.verify();
-//		console.log("SMTP server is ready");
+	const user = await insertUserPasswordVerification(
+		email,
+		password_verification_token_hash
+	);
 
-//		await emailTransporter.sendMail({
-//			from: process.env.EMAIL_USER,
-//			to: email,
-//			subject: "Verify your email",
+	const baseUrl = process.env.HOSTNAME;
+	const verificationUrl =
+	`https://${baseUrl}:8443/reset-password?token=${password_verification_token}`;
 
-//		html: `
-//			<h2>Welcome!</h2>
+	try {
+		await emailTransporter.verify();
+		await emailTransporter.sendMail({
+			from: process.env.EMAIL_USER,
+			to: email,
+			subject: "Reset your password",
 
-//			<p>Please verify your email.</p>
+		html: `
+			<h2>Hi!</h2>
 
-//			<p>
-//				<a href="${verificationUrl}">
-//					Verify Email
-//				</a>
-//			</p>
-//		`,
-//		});
-//		console.log("Email sent successfully!");
-//	} catch (err) {
-//		console.error("Failed to send email:", err);
-//	}
-//}
+			<p>Here is a link to reset your password.</p>
+
+			<p>
+				<a href="${verificationUrl}">
+					Reset password
+				</a>
+			</p>
+		`,
+		});
+		console.log("Email sent successfully!");
+		return {
+			ok: true,
+			statusCode: 200,
+			message:
+				"If that email exists, a password reset link has been sent.",
+		};
+	} catch (err) {
+		console.error("Failed to send email:", err);
+		return {
+			ok: false,
+			statusCode: 500,
+			message: "Could not send password reset email",
+		};
+	}
+}
